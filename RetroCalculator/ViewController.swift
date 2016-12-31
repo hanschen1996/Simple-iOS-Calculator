63e78738a922b722b96f3539b575dc836ad0507e
Junye Chen (Hans) <hans20030333@gmail.com>
2017-01-01 01:05:37 +0800

    initial working version

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,117 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class LaunchScreenVC: UIViewController {
+    
+}
 
+class calculatorVC: UIViewController {
+    
+    var btnSound: AVAudioPlayer!  // audio may or may not exist
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equal = "="
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav") // bundle resources for the app
+        let soundURL = URL(fileURLWithPath: path!) // set up the URL for the sound
+        
+        do { // catch if the sound has an error
+            try btnSound = AVAudioPlayer(contentsOf: soundURL) // import the sound
+            btnSound.prepareToPlay() // have the sound, prepare to play
+        } catch let err as NSError {
+            print(err.debugDescription) // print out the error
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberPressed(sender: UIButton) { // drag this line to every number, so that whenever a number is pressed, it can call this function
+        playSound()
+        
+        runningNumber += "\(sender.tag)" // get which number is pressed
+        outputLabel.text = runningNumber // display the number
     }
-
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: Operation.Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: Operation.Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: Operation.Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: Operation.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: Operation.Equal)
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
+    
+    func processOperation(operation: Operation) {
+        playSound()
+        if (currentOperation != Operation.Empty) { // already some operation pending, deal with current operation first
+            if (runningNumber != "") {  // has finished the set the second number, now do the operation
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                // do the operation first
+                if (currentOperation == Operation.Multiply) {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                    
+                } else if (currentOperation == Operation.Divide) {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                    
+                } else if (currentOperation == Operation.Subtract) {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                    
+                } else if (currentOperation == Operation.Add) {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                
+                //store the result
+                leftValStr = result
+                outputLabel.text = result
+            }
+            currentOperation = operation // set the new operation
+        } else { // the first operation, only set the leftVal
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+            
+            // um, what if user pressed an operation at the very beginning???
+        }
+    }
 
 }
 
